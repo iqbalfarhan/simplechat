5a056a2d7d6eaf2d834f6e17e4eb747d4ce092f1
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-05-21 17:02:11 +0800

    update user panel

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,8 +1,8 @@
 <div class="h-full">
     @isset($chat)
-        <div class="drawer drawer-open drawer-end h-full">
-            <input type="checkbox" id="userinfo" class="drawer-toggle" wire:model.live="sidebaruserinfo">
-            <div class="drawer-content p-5">
+        <div class="flex h-full">
+            <div class="p-5 flex-1">
+                <input type="checkbox" id="userinfo" class="hidden" wire:model.live="sidebaruserinfo">
                 @foreach ($datas as $data)
                     <div @class([
                         'chat',
@@ -18,16 +18,13 @@
                     </div>
                 @endforeach
             </div>
-            <div class="drawer-side">
-                <label for="userinfo" class="drawer-overlay"></label>
-                <ul @class([
-                    'menu border-l-2 border-base-300 h-full bg-base-100 transition-all',
-                    'w-80' => $sidebaruserinfo,
-                    'w-0 p-0' => !$sidebaruserinfo,
-                ])>
-                    <li><a href="">dfasd</a></li>
-                </ul>
-            </div>
+            <ul @class([
+                'menu border-l-2 border-base-300 min-h-full bg-base-100 transition-all',
+                'w-80' => $sidebaruserinfo,
+                'w-0 p-0' => !$sidebaruserinfo,
+            ])>
+                <li><a href="">dfasd</a></li>
+            </ul>
         </div>
     @else
         <div class="text-center opacity-50 flex justify-center items-center h-full max-w-sm mx-auto p-10">
